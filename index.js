aad6f5125c04e48e556cb46c65eecd64175b2908
Mark Hording <markhording@gmail.com>
2025-01-22 22:21:43 +0700

    Project complete

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
-const modalInner = document.getElementById('modal-inner');
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -12,8 +13,16 @@ modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
 
+declineBtn.addEventListener('mouseenter', function(){
+    modalChoiceBtns.classList.toggle('modal-btns-reverse')
+})
+
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
+
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
@@ -25,13 +34,16 @@ consentForm.addEventListener('submit', function(e){
         Making the sale...`
     }, 1500)
 
+
     setTimeout(function(){
         document.getElementById('modal-inner').innerHTML = `
-        <h2>Thanks you sucker! </h2>
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
         </div>
     `
+    modalCloseBtn.disabled = false
     }, 3000)
-  })
+
+})
